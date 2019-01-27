5f5e0cd75d5fd1c556a7f362a09a54769bbd2158
OmarTrasatti <omar@softinstigate.com>
2019-01-27 12:01:59 +0100

    added angular material, implemenete chat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
+import { SocketService } from './services/socket.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,11 +17,16 @@ import { ChatComponent } from './chat/chat.component';
     ChatComponent
   ],
   imports: [
+    MatToolbarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    MatFormFieldModule,
     AppRoutingModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
